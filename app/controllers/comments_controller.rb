61aaa3da530ef697c7a47adb64faf48545e5dbf2
ca0572 <102150395+ca0572@users.noreply.github.com>
2022-07-16 12:32:13 +0530

    story(AB#00000): for vivoapi

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,4 +20,25 @@ class CommentsController < ApplicationController
     def reply_flag_param
         params.permit(:reply_flag)
     end
+    # method for array data_type datapoints to make it optional
+  def array_datapoints_to_optional(array_datapoints_to_optional, touchpoint, required)
+    array_datapoints_to_optional.each do |dps|
+      arraydp = dps.split('/')
+      array_datapoint = nil
+      arraydp.each do |dp|
+        if arraydp.index(dp) == arraydp.length - 1 && (array_datapoint.nil? || array_datapoint.find_by(display_name: dp).present?)
+          array_datapoint = touchpoint.datapoints if array_datapoint.nil?
+          array_datapoint.find_by(display_name: dp).update!(required: required)
+        elsif array_datapoint.nil?
+          break unless touchpoint.datapoints.find_by(display_name: dp).present?
+
+          array_datapoint = touchpoint.datapoints.find_by(display_name: dp).children
+        else
+          break unless array_datapoint.find_by(display_name: dp).present?
+
+          array_datapoint = array_datapoint.find_by(display_name: dp).children
+        end
+      end
+    end
+  end
 end
